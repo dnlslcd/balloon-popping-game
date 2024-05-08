9c6fef8962ae373857a60e2f3d96d43518b6cb7f
dnlslcd <danielsalcedoq@gmail.com>
2024-05-08 22:00:42 +0200

    hecho todo menos los FALTA

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 // tu código aquí
+
+// Recupero todos los elementos del DOM con la class="balloon"
+const balloons = document.querySelectorAll('.balloon');
+console.log("🚀 ~ file: app.js:4 ~ balloons:", balloons);
+
+// defino una función para gestionar el evento mouseover
+function popBallon(event) {
+    console.log(event.target);
+
+    // 1. Debemos actualizar la propiedad event.target.textContent adecuadamente para simular que hemos hecho "POOP!" en el globo
+    event.target.textContent = "POP!";
+
+    // 2. Temos que actualizar los estilos en línea de event.target.style adecuadamente
+    event.target.style.background = "none";
+
+    // 3. Fijaos que el contador de globos que faltan por explotar, no funciona. Solucionadlo!
+    totalBalloonstoBePooped = totalBalloonstoBePooped - 1;
+    document.querySelector('#left').textContent = totalBalloonstoBePooped;
+
+    // PAra eliminar un listener de un elemento (el globo que acava de 'petar') tenemos que usar el método 
+    // El primer parámetro es el nombre del evento, y el segundo, el nombre de la función
+    event.target.removeEventListener('mouseover', popBallon);
+
+    // FALTA: Si el número de globos ha llegado a 0, debemos ocultar todos los balones y mostrar el div <div id="yay-no-balloons">
+    // FAlTA: mostrar la pantalla de vicoria al explotar todos los globos
+
+}
+
+// Voy a asociar a cada uno de estos elementos, un listener, para esuchar el evento mouseover
+// Lo que hacemos con forEach, es iterar por cada elemento del array de nodos, y asociarle un listener
+balloons.forEach(elem => {
+    elem.addEventListener('mouseover', popBallon);
+});
+
